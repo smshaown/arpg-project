57fccf05e8086ba49763aa719015a54b5a68647e
smshaown <smshaown65@gmail.com>
2023-12-21 09:52:22 +0600

    product details

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 
+	// SIDEBAR COLLAPSE
+	const toggleSidebar = document.querySelector('nav .toggle-sidebar');
+	const toggleCategories = document.querySelector('.toggle-categories');
+	const allSideDivider = document.querySelectorAll('#sidebar .divider');
+	const allDropdown = document.querySelectorAll('.dropdown'); 
+	const sidebar = document.getElementById('sidebar'); 
 
-  
-// SIDEBAR COLLAPSE
-const toggleSidebar = document.querySelector('nav .toggle-sidebar');
-const allSideDivider = document.querySelectorAll('#sidebar .divider');
+	// Function to toggle sidebar state
+	function toggleSidebarState() {
+	sidebar.classList.toggle('hide');
 
-if(sidebar.classList.contains('hide')) {
-	allSideDivider.forEach(item=> {
-		item.textContent = '-'
-	})
-	allDropdown.forEach(item=> {
+	if (sidebar.classList.contains('hide')) {
+		allSideDivider.forEach(item => {
+		item.textContent = '-';
+		});
+
+		allDropdown.forEach(item => {
 		const a = item.parentElement.querySelector('a:first-child');
 		a.classList.remove('active');
 		item.classList.remove('show');
-	})
-} else {
-	allSideDivider.forEach(item=> {
-		item.textContent = item.dataset.text;
-	})
-}
-
-toggleSidebar.addEventListener('click', function () {
-	sidebar.classList.toggle('hide');
-
-	if(sidebar.classList.contains('hide')) {
-		allSideDivider.forEach(item=> {
-			item.textContent = '-'
-		})
-
-		allDropdown.forEach(item=> {
-			const a = item.parentElement.querySelector('a:first-child');
-			a.classList.remove('active');
-			item.classList.remove('show');
-		})
+		});
 	} else {
-		allSideDivider.forEach(item=> {
-			item.textContent = item.dataset.text;
-		})
+		allSideDivider.forEach(item => {
+		item.textContent = item.dataset.text;
+		});
 	}
-})
+	}
 
+	
+	toggleCategories.addEventListener('click', function (event) {
+	event.preventDefault(); 
+	toggleSidebarState();
+	});
+
+	
+	toggleSidebar.addEventListener('click', function () {
+	toggleSidebarState();
+	});
 
 
 
@@ -73,5 +70,28 @@ function changeContent(content, clickedLink) {
   }
 }
 
+
+
+// product deatils 
+document.addEventListener('DOMContentLoaded', function () {
+	const showAllToggle = document.getElementById('showAllToggle');
+	const additionalOffersList = document.querySelectorAll('.flex.flex-row.items-center.gap-2.mb-1');
+  
+	for (let i = 3; i < additionalOffersList.length; i++) {
+	  additionalOffersList[i].classList.add('hidden');
+	}
+  
+	showAllToggle.addEventListener('click', function () {
+	  for (let i = 3; i < additionalOffersList.length; i++) {
+		additionalOffersList[i].classList.toggle('hidden');
+	  }
+  
+	  showAllToggle.classList.toggle('show-all');
+	  showAllToggle.classList.toggle('show-less');
+  
+	  const isShowingAll = showAllToggle.classList.contains('show-all');
+	  showAllToggle.textContent = isShowingAll ? 'Show Less' : 'Show All';
+	});
+  });
 
 
